69d2913671d084d420bd8ae72e5e15a8c2e3906c
Dario Caruso <darioyzf@gmail.com>
2015-10-04 16:40:12 +0200

    oki

--- tests/main.js
+++ tests/main.js
@@ -11,23 +11,27 @@ var over={
 
 };
 var oveer={
-  _id:'testt',
+  _id:'test',
   ff:"zz"
 };
 memrun.data("echo '{\"aa\":\"bb\"}'",function(data){
   verb(JSON.stringify(data),"debug","Memrun:data");
-});
-memrun.save("echo '{\"aa\":\"bb\"}'",testdb,{},function(data){
-  verb(JSON.stringify(data),"info","Memrun:save");
+  memrun.save("echo '{\"aa\":\"bb\"}'",testdb,{},function(data){
+    verb(JSON.stringify(data),"info","Memrun:save");
+    memrun.save("echo '{\"aa\":\"bb\"}'",testdb,over,function(data){
+      verb(JSON.stringify(data),"info","Memrun:save");
+      memrun.ifchange("echo '{\"aa\":\"bb\"}'",testdb,oveer,function(data){
+        verb(JSON.stringify(data),"debug","Memrun:change");
+        memrun.ifchange("echo '{\"aa\":\"bb\"}'",testdb,over,function(data){
+          verb(JSON.stringify(data),"debug","Memrun:change");
+          memrun.ifchange("echo '{\"aa\":\"bb\"}'",testdb,over,function(data){
+            verb(JSON.stringify(data),"error","Memrun:change");
+          });
+            rm.dir('./'+testdb)
+        });
+      });
 
-});
-memrun.save("echo '{\"aa\":\"bb\"}'",testdb,over,function(data){
-  verb(JSON.stringify(data),"info","Memrun");
 
-});
-memrun.ifchange("echo '{\"aa\":\"bb\"}'",testdb,over,function(data){
-  verb(JSON.stringify(data),"debug","Memrun");
-});
-memrun.ifchange("echo '{\"aa\":\"bb\"}'",testdb,oveer,function(data){
-  verb(JSON.stringify(data),"debug","Memrun:change");
+    });
+  });
 });
