6a30c47ec87f7dc12251642d3552899c48f70bb5
Dario Caruso <darioyzf@gmail.com>
2015-10-04 06:24:51 +0200

    oki

--- tests/main.js
+++ tests/main.js
@@ -1,7 +1,8 @@
 var memrun = require('../index.js'),
 verb=require('verbo'),
 testdb='testdb',
-db=require('pouchdb')(testdb);
+db=require('pouchdb')(testdb),
+rm=require('rm-r');
 
 
 var over={
@@ -10,42 +11,42 @@ var over={
 var oveer={
   _id:'testt'
 };
-memrun.data("echo '{aa:bb}'",function(data){
-  verb(data,"info","Memrun");
-});
-memrun.save("echo '{\"aa\":\"bb\"}'",testdb,{},function(data){
-  verb(JSON.stringify(data),"debug","Memrun");
-db.allDocs({include_docs:true}).then(function(doc){
-  for(var i=0;i<doc.rows.length;i++){
-    verb(JSON.stringify(doc.rows[i].doc))
-
-  }
-})
-});
-memrun.save("echo '{\"aa\":\"bb\"}'",testdb,over,function(data){
-  verb(JSON.stringify(data),"debug","Memrun");
-db.allDocs({include_docs:true}).then(function(doc){
-  for(var i=0;i<doc.rows.length;i++){
-    verb(JSON.stringify(doc.rows[i].doc))
-
-  }
-})
-});
+// memrun.data("echo '{aa:bb}'",function(data){
+//   verb(data,"info","Memrun");
+// });
+// memrun.save("echo '{\"aa\":\"bb\"}'",testdb,{},function(data){
+//   verb(JSON.stringify(data),"debug","Memrun");
+// db.allDocs({include_docs:true}).then(function(doc){
+//   for(var i=0;i<doc.rows.length;i++){
+//     verb(JSON.stringify(doc.rows[i].doc))
+//
+//   }
+// })
+// });
+// memrun.save("echo '{\"aa\":\"bb\"}'",testdb,over,function(data){
+//   verb(JSON.stringify(data),"debug","Memrun");
+// db.allDocs({include_docs:true}).then(function(doc){
+//   for(var i=0;i<doc.rows.length;i++){
+//     verb(JSON.stringify(doc.rows[i].doc))
+//
+//   }
+// })
+// });
+// memrun.ifchange("echo '{\"aa\":\"bb\"}'",testdb,oveer,function(data){
+//   verb(JSON.stringify(data),"debug","Memrun");
+// db.allDocs({include_docs:true}).then(function(doc){
+//   for(var i=0;i<doc.rows.length;i++){
+//     verb(JSON.stringify(doc.rows[i].doc))
+//
+//   }
+// })
+// });
 memrun.ifchange("echo '{\"aa\":\"bb\"}'",testdb,oveer,function(data){
   verb(JSON.stringify(data),"debug","Memrun");
 db.allDocs({include_docs:true}).then(function(doc){
   for(var i=0;i<doc.rows.length;i++){
     verb(JSON.stringify(doc.rows[i].doc))
-
-  }
-})
-});
-memrun.ifchange("echo '{\"aa\":\"bb\"}'",testdb,oveer,function(data){
-  verb(JSON.stringify(data),"debug","Memrun");
-db.allDocs({include_docs:true}).then(function(doc){
-  for(var i=0;i<doc.rows.length;i++){
-    verb(JSON.stringify(doc.rows[i].doc))
-
+rm.dir('./'+testdb);
   }
 })
 });
